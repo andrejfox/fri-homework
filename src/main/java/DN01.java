072c249312c6d9bbc18ad092c7aafcfad575a63f
andrejfox <andrej.vencelj.007@gmail.com>
2025-10-30 08:12:57 +0100

    Tom fix to DN01

--- src/main/java/DN01.java
+++ src/main/java/DN01.java
@@ -10,18 +10,14 @@ public class DN01 {
         // add all 1x1 squares
         int sqCount = (a - 1) * (b - 1);
 
-        // if a & b form a square add 1 to sqCount
-        if (a == b) {
-            sqCount++;
-        } else {
-            // sets an as the bigger value
-            int hold = a;
 
-            if (a < b) {
-                a = b;
-                b = hold;
-            }
+        // sets an as the bigger value
+        int hold = a;
+        if (a < b) {
+            a = b;
+            b = hold;
         }
+
 
         for (int i = 2; i <= b; i++) {
             sqCount += (a - i) * (b - i);
